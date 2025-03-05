4c7cfb07c055f6387862c295c2f1b7415c298e95
oleksandr.o.burlakov <oleksandr.o.burlakov@gmail.com>
2025-03-05 22:18:08 +0200

    Init vue commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import { Menubar, Button } from 'primevue';
+</script>
+
+<template>
+  <div class="flex flex-col w-full p-2">
+    <header class="w-full">
+      <Menubar>
+        <template #start>
+          <div class="wrapper">
+            <nav>
+              <RouterLink to="/">
+                <Button label="Home" variant="link" />
+              </RouterLink>
+              <RouterLink to="/about">
+                <Button label="About" variant="link" />
+              </RouterLink>
+            </nav>
+          </div>
+        </template>
+      </Menubar>
+    </header>
+    <div class="flex">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  something about my site
+
+</template>
+
+<style scoped>
+
+</style>
